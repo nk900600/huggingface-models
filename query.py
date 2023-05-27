5951b9205c2b7f1e7421edb9359665c4a8b846bf
Nikhil Kumar <nk90600@gmail.com>
2023-05-27 21:21:11 +0530

    VM commit

--- query.py
+++ query.py
@@ -16,10 +16,10 @@ embeddings_model_name =EMBEDDINGS_MODEL_NAME_VM
 # embeddings_model_name =EMBEDDINGS_MODEL_NAME
 persist_directory = PERSIST_DIRECTORY
 
-# model_type = MODEL_TYPE
-model_type = MODEL_TYPE_VM
-# model_path = MODEL_PATH
-model_path = MODEL_PATH_VM
+model_type = MODEL_TYPE
+# model_type = MODEL_TYPE_VM
+model_path = MODEL_PATH
+# model_path = MODEL_PATH_VM
 model_n_ctx = MODEL_N_CTX
 
 
